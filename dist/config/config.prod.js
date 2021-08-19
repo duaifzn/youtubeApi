c1b48d2838af8ae959a62dc2c2bef8f02020644c
duaifzn <duaifzn@gmail.com>
2021-08-19 10:14:50 +0800

    chore: modify npm command

--- dist/config/config.prod.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prod = void 0;
-exports.prod = {
-    youtubeKey: 'AIzaSyBKgc_Q_DYQ6kDUAQMPKcbzK75BngqvKUs',
-};
-//# sourceMappingURL=config.prod.js.map
